7c6a823c4a758b8b73e247516bc5d9589c90f567
vipul.potdar <vipul.potdar@VIPO-BLR-01.accolite.com>
2018-08-31 13:20:19 +0530

    final

--- src/main/java/com/prud/mapper/impl/OrikaModelNewBusinessMapperImpl.java
+++ src/main/java/com/prud/mapper/impl/OrikaModelNewBusinessMapperImpl.java
@@ -2,7 +2,6 @@ package com.prud.mapper.impl;
 
 import java.util.Map;
 
-import com.prud.constant.IntegrationConstants;
 import com.prud.mapper.ModelMapper;
 import com.prud.mapper.customconverter.AssigneeDetailsCustomConverter;
 import com.prud.mapper.customconverter.BankDetailsDateFromCustomConverter;
@@ -13,6 +12,7 @@ import com.prud.mapper.customconverter.CoverageDetailsCustomConverter;
 import com.prud.mapper.customconverter.LifeDetailsCustomConverter;
 import com.prud.mapper.customconverter.MandatoryEffectiveDateCustomConvertor;
 import com.prud.mapper.customconverter.PreminumTransactionDateCustomConvertor;
+import com.prud.mapper.customconverter.ProposalDateCustomConvertor;
 
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
@@ -39,8 +39,9 @@ public class OrikaModelNewBusinessMapperImpl implements ModelMapper {
 		
 		converterFactory.registerConverter("bankDetailsDateFromCC", new BankDetailsDateFromCustomConverter());
 		converterFactory.registerConverter("billingRenewalDateCC", new BillingRenewalDateCustomConverter());
-		converterFactory.registerConverter("mandatoryEffectiveCC", new MandatoryEffectiveDateCustomConvertor());
-		converterFactory.registerConverter("preminumTransactionCC", new PreminumTransactionDateCustomConvertor());
+		converterFactory.registerConverter("mandatoryEffectiveDateCC", new MandatoryEffectiveDateCustomConvertor());
+		converterFactory.registerConverter("preminumTransactionDateCC", new PreminumTransactionDateCustomConvertor());
+		converterFactory.registerConverter("proposalDateCC", new ProposalDateCustomConvertor());
 	}
 
 	@SuppressWarnings("unchecked")
@@ -52,15 +53,17 @@ public class OrikaModelNewBusinessMapperImpl implements ModelMapper {
 				classMapBilder.field(enrty.getKey(), enrty.getValue());
 			}
 		}
-	     classMapBilder.fieldMap("assignees.assigneeDetails", "assignees.nbscrtiassigneedetails").converter("assigneeCC").add()
+	    classMapBilder.fieldMap("assignees.assigneeDetails", "assignees.nbscrtiassigneedetails").converter("assigneeCC").add()
 		.fieldMap("beneficiary.beneficiaryDetails", "beneficiary.nbscrtibendetails").converter("beneficiaryCC").add()
 		.fieldMap("clientDetails", "nbscrticlient").converter("clientDetailsCC").add()
 		.fieldMap("coverageDetails", "nbscrticoveragedetails").converter("coverageDetailsCC").add()
 		.fieldMap("lifeDetails", "nbscrtilives").converter("lifeDetailsCC").add()
 		.fieldMap("bankDetails.datefrom", "nbscrtibankdetails.datefrom").converter("bankDetailsDateFromCC").add()
 		.fieldMap("contractDetails.billingRenewalDate", "nbscrticontracthdrdetails.billcd").converter("billingRenewalDateCC").add()
-		.fieldMap("mandateDetails.effectiveDate", "nbscrtimandatedetails.effdate").converter("mandatoryEffectiveCC").add()
-		.fieldMap("premiumRCTDetails.tranDate", "nbscrtipremiumrctdetails.trandatex").converter("preminumTransactionCC").add()
+		.fieldMap("mandateDetails.effectiveDate", "nbscrtimandatedetails.effdate").converter("mandatoryEffectiveDateCC").add()
+		.fieldMap("premiumRCTDetails.tranDate", "nbscrtipremiumrctdetails.trandatex").converter("preminumTransactionDateCC").add()
+		.fieldMap("contractDetails.proposalDate", "nbscrticontracthdrdetails.hpropdte").converter("proposalDateCC").add()
+		
 		.byDefault().register();
 		mapper = mapperFactory.getMapperFacade();
 		return mapper.map(source, targetClass);
