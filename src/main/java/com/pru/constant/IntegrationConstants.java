7f7b08414ec5bc16e3523539fc60d793a814ae7e
vipul.potdar <vipul.potdar@VIPO-BLR-01.accolite.com>
2018-09-14 18:03:26 +0530

    file seperator

--- src/main/java/com/pru/constant/IntegrationConstants.java
+++ src/main/java/com/pru/constant/IntegrationConstants.java
@@ -1,5 +1,7 @@
 package com.pru.constant;
 
+import java.io.File;
+
 public class IntegrationConstants {
 	private IntegrationConstants() {
 
@@ -19,16 +21,16 @@ public class IntegrationConstants {
 	public static final String FOLDERNAME_XSL = "/xsl/";
 	public static final String CLIENT_CREATE_REQUEST_PARAMETER_VALUE = "U";
 	public static final String CLIENT_CREATE_REQUEST_PARAMETER_NAME = "COMPANY";
-	public static final String NBS_XSLT_FILE_NAME = "\\src\\main\\resources\\newbusiness-mapper\\newBusinessProposal.xsl";
-	public static final String CLI_XSLT_FILE_NAME = "\\src\\main\\resources\\client-mapper\\createClient.xsl";
-	public static final String FLINK_KAFKA_CONFIG_LOCATION = "\\src\\main\\resources\\flink-config\\flink-kafka-config.properties";
+	public static final String NBS_XSLT_FILE_NAME = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"newbusiness-mapper"+File.separator+"newBusinessProposal.xsl";
+	public static final String CLI_XSLT_FILE_NAME = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"client-mapper"+File.separator+"createClient.xsl";
+	public static final String FLINK_KAFKA_CONFIG_LOCATION = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"flink-config"+File.separator+"flink-kafka-config.properties";
 	public static final String CLIENT_URL = "http://10.163.177.100:9081/FSUWebSrv/CLIService";
 	public static final String NEW_BUSINESS_URL = "http://10.163.177.100:9081/LiFEWebServices/NBSService";
 
-	public static final String IL_CONFIG_PROP_LOCATION = "\\src\\main\\resources\\il-config\\il-config.properties";
-	public static final String PROPOSAL_TO_CLIENT_PROP_LOCATION = "\\src\\main\\resources\\client-mapper\\proposal-to-client-mapping.properties";
-	public static final String PROPOSAL_TO_NEWBIZMAPPING_PROP_LOCATION = "\\src\\main\\resources\\newbusiness-mapper\\proposal-to-newbusiness-mapping.properties";
+	public static final String IL_CONFIG_PROP_LOCATION = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"il-config"+File.separator+"il-config.properties";
+	public static final String PROPOSAL_TO_CLIENT_PROP_LOCATION = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"client-mapper"+File.separator+"proposal-to-client-mapping.properties";
+	public static final String PROPOSAL_TO_NEWBIZMAPPING_PROP_LOCATION = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"newbusiness-mapper"+File.separator+"proposal-to-newbusiness-mapping.properties";
 
-	public static final String PROPOSAL_TO_CLIENT_CUSTOM_PROP_LOCATION = "\\src\\main\\resources\\client-mapper\\proposal-to-client-custom-mapping.properties";
-	public static final String PROPOSAL_TO_NEWBIZMAPPING_CUSTOM_PROP_LOCATION = "\\src\\main\\resources\\newbusiness-mapper\\proposal-to-newbusiness-custom-mapping.properties";
+	public static final String PROPOSAL_TO_CLIENT_CUSTOM_PROP_LOCATION = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"client-mapper"+File.separator+"proposal-to-client-custom-mapping.properties";
+	public static final String PROPOSAL_TO_NEWBIZMAPPING_CUSTOM_PROP_LOCATION = File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"newbusiness-mapper"+File.separator+"proposal-to-newbusiness-custom-mapping.properties";
 }
